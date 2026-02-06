10b01315ce779503421dd117833bd4e54ce0309e
Anvitha L <anvitha@gmail.com>
2026-02-06 12:15:24 +0530

    Day 5

---
+++ main.py
@@ -0,0 +1,10 @@
+import math_operations
+
+# Calculate 2^10
+result_power = math_operations.power(2, 10)
+
+# Calculate average of the list
+result_avg = math_operations.average([10, 20, 30, 40])
+
+print("2^10 =", result_power)
+print("Average =", result_avg)
