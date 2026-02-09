8720eb837eb5982491ab7253acbe4cc887acd679
Anvitha L <anvitha@gmail.com>
2026-02-09 15:12:54 +0530

    day7

---
+++ data_cleaning.py
@@ -0,0 +1,6 @@
+with open("data.txt", "r") as file:
+    for line in file:
+        if line.strip():                 
+            cleaned_line = line.strip()  
+            print(cleaned_line.lower())
+            print(cleaned_line.upper())
